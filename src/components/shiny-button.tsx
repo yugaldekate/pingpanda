618d19b0b88cc47de6a322660bd5026b4b27a371
Yugal D Dekate <yugaldekate72@gmail.com>
2025-05-31 22:45:04 +0530

    created navbar, installed shadn and clerk

--- src/components/shiny-button.tsx
+++ src/components/shiny-button.tsx
@@ -1,7 +1,7 @@
-import { cn } from "@/utils"
-import { ArrowRight } from "lucide-react"
-import Link from "next/link"
-import { AnchorHTMLAttributes } from "react"
+import Link from "next/link";
+import { AnchorHTMLAttributes } from "react";
+import { cn } from "@/utils";
+import { ArrowRight } from "lucide-react";
 
 interface ShinyButtonProps extends AnchorHTMLAttributes<HTMLAnchorElement> {
 
@@ -15,7 +15,7 @@ export const ShinyButton = ({ className, children, href, ...props }: ShinyButton
                 "group relative flex items-center justify-center gap-2 overflow-hidden whitespace-nowrap rounded-md border border-white bg-brand-700 px-8 text-base/7 font-medium text-white transform transition-all duration-300 hover:ring-2 hover:ring-brand-700 hover:ring-offset-2 focus:outline-none focus:ring-2 focus:ring-offset-2",
                 className
             )}
-        {...props}
+            {...props}
         >
             <span className="relative z-10 flex items-center gap-2">
                 {children}
