527a90260e6704803c86f6f5910474762801c18b
Yugal D Dekate <yugaldekate72@gmail.com>
2025-06-06 20:09:54 +0530

    created DB models, saved sign-up users to DB

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,19 +1,19 @@
-import { Hono } from "hono"
-import { cors } from "hono/cors"
-import { handle } from "hono/vercel"
-import { postRouter } from "./routers/post-router"
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { handle } from "hono/vercel";
+import { authRouter } from "./routers/auth-router";
 
-const app = new Hono().basePath("/api").use(cors())
+const app = new Hono().basePath("/api").use(cors());
 
 /**
  * This is the primary router for your server.
  *
  * All routers added in /server/routers should be manually added here.
  */
-const appRouter = app.route("/post", postRouter)
+const appRouter = app.route("/auth", authRouter);
 
 // The handler Next.js uses to answer API requests
-export const httpHandler = handle(app)
+export const httpHandler = handle(app);
 
 /**
  * (Optional)
@@ -21,7 +21,7 @@ export const httpHandler = handle(app)
  *
  * Run `npm run deploy` for one-click API deployment to Cloudflare's edge network
  */
-export default app
+export default app;
 
 // export type definition of API
-export type AppType = typeof appRouter
+export type AppType = typeof appRouter;
---
+++ src/server/routers/auth-router.ts
@@ -0,0 +1,38 @@
+import { db } from "@/db";
+import { currentUser } from "@clerk/nextjs/server";
+
+import { publicProcedure } from "../procedures";
+
+import { router } from "../__internals/router";
+
+export const dynamic = "force-dynamic";
+
+export const authRouter = router({
+    getDatabaseSyncStatus: publicProcedure.query(async ({ c, ctx }) => {
+        const auth = await currentUser();
+
+        if (!auth) {
+            return c.json({ isSynced: false });
+        }
+
+        const user = await db.user.findFirst({
+            where: { 
+                externalId: auth.id,
+            },
+        });
+
+        console.log('USER IN DB:', user);
+
+        if (!user) {
+            await db.user.create({
+                data: {
+                    quotaLimit: 100,
+                    externalId: auth.id,
+                    email: auth.emailAddresses[0].emailAddress,
+                },
+            });
+        }
+
+        return c.json({ isSynced: true });
+    }),
+});
