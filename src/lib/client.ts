870c035015f1dfbf254ad5c4d5997ad57fb135f2
Yugal D Dekate <yugaldekate72@gmail.com>
2025-06-18 19:32:00 +0530

    modified the client.ts

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,7 +1,7 @@
 import { AppType } from "@/server"
 import { hc } from "hono/client"
 import { HTTPException } from "hono/http-exception"
-import { StatusCode } from "hono/utils/http-status"
+import { ContentfulStatusCode } from "hono/utils/http-status"
 import superjson from "superjson"
 
 const getBaseUrl = () => {
@@ -29,7 +29,7 @@ export const baseClient = hc<AppType>(getBaseUrl(), {
     const response = await fetch(input, { ...init, cache: "no-store" })
 
     if (!response.ok) {
-      throw new HTTPException(response.status as StatusCode, {
+      throw new HTTPException(response.status as ContentfulStatusCode, {
         message: response.statusText,
         res: response,
       })
